2fa2ed6ab07d6e01daec92ea2e6d1bf53c6f4fe1
Christopher Guest <chrisguest75@users.noreply.github.com>
2022-06-12 19:55:06 +0000

    Add a typescript template and attempt to get homebrew working

--- 01_bash_container/18_bats_mock/configure.sh
+++ 01_bash_container/18_bats_mock/configure.sh
@@ -5,6 +5,6 @@ which bats
 
 git clone https://github.com/bats-core/bats-support test/test_helper/bats-support
 git clone https://github.com/bats-core/bats-assert test/test_helper/bats-assert  
+git clone https://github.com/grayhemp/bats-mock test/test_helper/bats-mock
 
 ./run_tests.sh
-
